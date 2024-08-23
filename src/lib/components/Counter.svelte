69f78d9922cc94d664992ef89677e56323a352d8
mah <mahsa.79mirmiran@gmail.com>
2024-08-23 22:07:33 +0300

    svelte kit section 2 finished

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	export let initialcont: number;
+	export let maxCount: number;
+	let message: string = '';
+
+	let count1 = initialcont;
+
+	function increment() {
+		if (count1 === maxCount) {
+			message = `Max count reached: ${maxCount}`;
+			return;
+		}
+		count1 += 1;
+		message = '';
+	}
+</script>
+
+<div class="flex justify-between items-center w-full">
+	<button
+		class="p-3 variant-ghost-primary
+"
+		on:click={increment}
+	>
+		Click to count up 1
+	</button>
+
+	{#if message}
+		<p>{message}</p>
+	{:else}
+		<h1>
+			{count1}
+		</h1>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,17 @@
+<script>
+	import Counter from '$lib/components/Counter.svelte';
+
+	const props = {
+		initialcont: 4,
+		maxCount: 12
+	};
+</script>
+
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
+		<Counter {...props} />
 		<h2 class="h2">Welcome to Skeleton.</h2>
 		<!-- Animated Logo -->
 		<figure>
@@ -19,12 +29,7 @@
 		</figure>
 		<!-- / -->
 		<div class="flex justify-center space-x-2">
-			<a
-				class="btn variant-filled"
-				href="https://skeleton.dev/"
-				target="_blank"
-				rel="noreferrer"
-			>
+			<a class="btn variant-filled" href="https://skeleton.dev/" target="_blank" rel="noreferrer">
 				Launch Documentation
 			</a>
 		</div>
@@ -46,7 +51,8 @@
 	}
 	.img-bg {
 		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+		animation:
+			pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
 			glow 5s linear infinite;
 	}
 	@keyframes glow {
